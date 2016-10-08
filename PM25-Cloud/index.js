05c6ef4ab6ad692a7aeed86c88b8b9826686c87f
zhangleigang <zhangleigang@jd.com>
2016-10-08 20:11:44 +0800

    babel-register的用法修正

--- PM25-Cloud/index.js
+++ PM25-Cloud/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-require('babel/register')({ stage: 0 });
+require('babel-register');
 
 var debug = require('debug')('PM2.5');
 var app = require('./app');
 
 app.set('port', process.env.PORT || 8000);
-app.set('env', 'production');
 
 var server = app.listen(app.get('port'), function() {
     debug('Express server listening on port ' + server.address().port);
